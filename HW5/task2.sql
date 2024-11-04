575dc73b243258904a6b905080e0c4f91b4f66d7
Reuben Waysome <reuben.waysome@city.ac.uk>
2024-11-04 00:01:08 +0000

    Update task2.sql

--- HW5/task2.sql
+++ HW5/task2.sql
@@ -102,32 +102,23 @@ INSERT INTO restBill VALUES
     (00022, 160312, 545.01, 'Sunil Shah', 4, 006, 004);
 
 -- 1
-
 SELECT SUM(bill_total) AS 'Income'
 FROM restBill;
-
 -- 2
-
 SELECT SUM(bill_total) AS 'Feb Income'
 FROM restBill
 WHERE bill_date > 160200 AND bill_date < 160300;
-
 -- 3
-
 SELECT AVG(bill_total)
 FROM restBill
 WHERE table_no = 2;
-
 -- 4
-
 SELECT MIN(no_of_seats) as 'Min',
 	MAX(no_of_seats) as 'Max',
     AVG(no_of_seats) as 'Avg'
 FROM restRest_table
 WHERE room_name = 'Blue';
-
 -- 5
-
 SELECT COUNT(DISTINCT(table_no))
 FROM restBill
 WHERE waiter_no = 002 OR waiter_no = 004;
