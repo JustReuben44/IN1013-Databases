e2a955039ec11c11255f227fdd1a70584170f388
Reuben Waysome <reuben.waysome@city.ac.uk>
2024-11-04 00:00:30 +0000

    Update task1.sql

--- HW5/task1.sql
+++ HW5/task1.sql
@@ -102,38 +102,27 @@ INSERT INTO restBill VALUES
     (00022, 160312, 545.01, 'Sunil Shah', 4, 006, 004);
 
 -- 1
-
 SELECT bill_date, bill_total
 FROM restBill
 WHERE cust_name = 'Bob Crow';
-
 -- 2
-
 SELECT DISTINCT cust_name
 FROM restBill
 WHERE cust_name LIKE '%Smith%'
 ORDER BY cust_name DESC;
-
 -- 3
-
 SELECT DISTINCT cust_name
 FROM restBill
 WHERE cust_name LIKE '% %C%';
-
 -- 4
-
 SELECT first_name
 FROM restStaff
 WHERE headwaiter IS null;
-
 -- 5
-
 SELECT *
 FROM restBill
 WHERE bill_date > 160200 AND bill_date < 160300;
-
 -- 6
-
 SELECT DISTINCT bill_date
 FROM restBill
 WHERE bill_date > 150000 AND bill_date < 160000;
