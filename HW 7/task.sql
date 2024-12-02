5abaf8e76cd42d05beb27e1b90426d96d42a9caf
Reuben Waysome <reuben.waysome@city.ac.uk>
2024-12-02 21:51:04 +0000

    Update task.sql

--- HW 7/task.sql
+++ HW 7/task.sql
@@ -1,1 +1,25 @@
-
+-- 1
+CREATE VIEW samsBills AS
+SELECT first_name, surname, bill_date, cust_name, bill_total
+FROM restBill B
+JOIN restStaff S ON B.waiter_no = S.staff_no
+WHERE first_name = "Sam" AND surname = "Pitt";
+-- 2
+SELECT * 
+FROM samsBills
+WHERE bill_total > 400;
+-- 3
+CREATE VIEW roomTotals AS
+SELECT RM.room_name, SUM(bill_total) AS total_sum
+FROM restRoom_management RM
+JOIN restRest_table RT ON RM.room_name = RT.room_name
+JOIN restBill B ON RT.table_no = B.table_no
+GROUP BY RM.room_name;
+-- 4
+CREATE VIEW teamTotals AS
+SELECT CONCAT(S1.first_name, ' ', S1.surname) AS headwaiter_name, 
+    SUM(bill_total) AS total_sum
+FROM restStaff S1
+JOIN restStaff S2 ON S1.staff_no = S2.headwaiter
+JOIN restBill B ON S2.staff_no = B.waiter_no
+GROUP BY S1.staff_no, S1.first_name, S1.surname;
